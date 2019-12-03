0f627a910b1c672b5b87079d5555fe6d5882330c
Nathan Carlson <nathan.l.carlson@outlook.com>
2019-12-02 16:15:51 -0800

    Create directory for holding build results

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-GOOS=darwin go build -o sproket-darwin cmd/sproket/main.go
-GOOS=linux go build -o sproket-linux cmd/sproket/main.go
+GOOS=darwin go build -o build/sproket-darwin cmd/sproket/main.go
+GOOS=linux go build -o build/sproket-linux cmd/sproket/main.go
