163050c5d6a05f0cfea602e22424c53d67f0f927
Nathan Carlson <nathan.l.carlson@outlook.com>
2019-12-02 16:58:51 -0800

    Keep a note of supported OS/ARCH combinations for compilation

--- build.sh
+++ build.sh
@@ -1,2 +1,19 @@
+# A list of 64-bit GOOS/GOARCH supported by go out of the box
+# darwin/amd64
+# dragonfly/amd64
+# freebsd/amd64
+# linux/amd64
+# linux/arm64
+# linux/ppc64
+# linux/ppc64le
+# linux/mips64
+# linux/mips64le
+# linux/s390x
+# netbsd/amd64
+# openbsd/amd64
+# plan9/amd64
+# solaris/amd64
+# windows/amd64
+
 GOOS=darwin go build -o build/sproket-darwin cmd/sproket/main.go
 GOOS=linux go build -o build/sproket-linux cmd/sproket/main.go
